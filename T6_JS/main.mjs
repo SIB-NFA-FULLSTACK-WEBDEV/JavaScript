f2f2fe96f316c20bf553df60b26b9da581867cfd
Ramdhan Ariansyah <160679262+ramdhanarrr@users.noreply.github.com>
2025-03-26 08:55:58 +0700

    tugas 6 js

--- T6_JS/main.mjs
+++ T6_JS/main.mjs
@@ -4,8 +4,8 @@ import {index, store, destroy} from './controller.mjs';
 
 const main = () => {
     //Tambah dua data baru
-    store({nama: 'Data 11', umur: 30, alamat: 'Jl. Data 11'});
-    store({nama: 'Data 12', umur: 31, alamat: 'Jl. Data 12'});
+    store({nama: 'Data 11', umur: 30, alamat: 'Jl. Data 11', email: 'data11@gmail.com'});
+    store({nama: 'Data 12', umur: 31, alamat: 'Jl. Data 12', email: 'data12@gmail.com'});
 
     //Tampilkan data
     index();
